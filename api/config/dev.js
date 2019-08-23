72e4e1d3e94fca7eb18e7d84c6e74b2ee4752714
Sean <snpdolan@gmail.com>
2019-08-22 22:05:03 -0400

    Delete dev.js

--- api/config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb://localhost/minddesign',
-  TOKEN_SECRET: 'greatstufftomake',
-  AWS_ACCESS_KEY_ID: 'AKIAIQ3YDR3THJJJBDXA',
-  AWS_SECRET_ACCESS_KEY: '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',
-  S3_BUCKET_NAME: 'minddesign-assets'
-};
