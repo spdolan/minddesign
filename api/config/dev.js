2387663f96e87386ea60214af9488cdfa74801cf
snpdolan <snpdolan@gmail.com>
2019-08-25 23:05:58 -0400

    wow, just wow.

--- api/config/dev.js
+++ api/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   MONGODB_URI: 'mongodb://localhost/minddesign',
   TOKEN_SECRET: 'greatstufftomake',
-  AWS_ACCESS_KEY_ID: 'AKIAJCNKLUTU55EUJRYA',
-  AWS_SECRET_ACCESS_KEY: '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',
+  AWS_ACCESS_KEY_ID: 'AKIAIFJ4KJBTOM6EFDJQ',
+  AWS_SECRET_ACCESS_KEY: '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',
   S3_BUCKET_NAME: 'minddesign-assets'
 };
