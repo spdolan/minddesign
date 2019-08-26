639f1ce1c2088db7fa2c3bbfd6c42994b9656114
snpdolan <snpdolan@gmail.com>
2019-08-25 22:45:46 -0400

    updated for proper cloud things.

--- api/config/dev.js
+++ api/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   MONGODB_URI: 'mongodb://localhost/minddesign',
   TOKEN_SECRET: 'greatstufftomake',
-  AWS_ACCESS_KEY_ID: 'AKIAIQHH7XR5AYHRJIEQ',
-  AWS_SECRET_ACCESS_KEY: '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',
+  AWS_ACCESS_KEY_ID: 'AKIAJCNKLUTU55EUJRYA',
+  AWS_SECRET_ACCESS_KEY: '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',
   S3_BUCKET_NAME: 'minddesign-assets'
 };
